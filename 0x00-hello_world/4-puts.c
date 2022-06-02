c68d119d63302d4a638b6aaf3eb8ebed844500bc
Gnalynn <situmalynn9@gmail.com>
2022-06-02 14:23:49 -0700

    Task 1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Does main stuff
+*
+*
+* Return: Always (0) Success
+*/
+int main(void)
+{
+	puts ("\"programming is like building a multilingual puzzle");
+	return (0);
+}
