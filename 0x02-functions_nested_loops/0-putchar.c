671fb77f8ddc5dab1ad7c7d51acd96f39e06768d
Gnalynn <situmalynn9@gmail.com>
2022-06-08 09:53:47 -0700

    nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,15 +9,13 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	int i;
+	char str[] = "_putchar";
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(*(str + i));
+	}
 	_putchar('\n');
 	return (0);
 }
