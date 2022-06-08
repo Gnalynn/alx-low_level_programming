adbafdb2febed64c795dee2a72fea1e837d6b8a7
Gnalynn <situmalynn9@gmail.com>
2022-06-08 13:46:02 -0700

    nested loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,13 @@
 #include "main.h"
-
 /**
-
  * print_alphabet_x10 - Prints alphabet x 10.
-
- *
-
  * Return: Always 0.
-
  */
 
 void print_alphabet_x10(void)
 
 {
-
 	int i = 0;
-
 	char az = 'a';
 
 
