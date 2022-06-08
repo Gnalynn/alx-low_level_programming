378369214dc5b6cb55bf62945fdf65bbe1a8d2a3
Gnalynn <situmalynn9@gmail.com>
2022-06-08 14:04:22 -0700

    nested loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,23 +9,12 @@ void print_alphabet_x10(void)
 {
 	int i = 0;
 	char az = 'a';
-
-
-
 	for (i = 0; i < 10; i++)
-
 	{
-
 		for (az = 'a'; az <= 'z'; az++)
-
 		{
-
 			_putchar(az);
-
 		}
-
 		_putchar('\n');
-
 	}
-
 }
