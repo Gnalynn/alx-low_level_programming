d2f99cc5b8341562efd97a2861fb3427c0332466
Gnalynn <situmalynn9@gmail.com>
2022-06-22 20:32:37 +0300

    recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * is_square_root - determines the square root of
+ * @j: an input value
+ * @i: input value
+ *
+ * Description: determines the square root
+ * Return: returns the input val
+ */
+
+int is_square_root(int i, int j)
+{
+	if (j * j > i)
+	{
+		return (-1);
+	}
+	if (j * j == i)
+	{
+		return (j);
+	}
+	return (is_square_root(i, j + 1));
+}
+
