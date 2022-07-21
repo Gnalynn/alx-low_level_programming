d72654f992715a2803aecc26f22554d945ce6d9f
Gnalynn <situmalynn9@gmail.com>
2022-07-21 22:41:27 +0300

    binary

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,23 +1,19 @@
 #include "main.h"
-
+#include "2-get_bit.c"
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string
- * Return: 0;
+ * clear_bit - clears index to 0
+ * @n: integer
+ * @index: unsigned int
+ * Return: Always 0
  */
-unsigned int binary_to_uint(const char *b)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-	int i;
-	unsigned int c = 0;
+	if (index > 32)
+		return (-1);
 
-	if (!b)
-		return (0);
+	(*n) &= ~(1 << index);
 
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		c = 2 * c + (b[i] - '0');
-	}
-	return (c);
+	if (get_bit((*n), index) == 0)
+		return (1);
+	return (-1);
 }
