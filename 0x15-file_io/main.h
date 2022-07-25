2196f81c707c949930c61f1250c7be096e188c0f
Gnalynn <situmalynn9@gmail.com>
2022-07-25 21:41:54 +0300

    tread slightly

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -27,9 +27,3 @@ void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 #endif
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Securi
